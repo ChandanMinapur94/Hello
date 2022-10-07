3f634ef6f091d1bb35ebc4cf8d1050d9bd1e537e
ChandanMilki94 <chandra121000.ck@gmail.com>
2022-10-07 21:52:35 +0530

    move

---
+++ src/OnlyJava/A.java
@@ -0,0 +1,10 @@
+package src.OnlyJava;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/OnlyJava/B.java
@@ -0,0 +1,9 @@
+package src.OnlyJava;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
